453b971e1709e1120047e419052b59aec26b91f7
Hieu <hoangtrunghieu1441998@gmail.com>
2020-09-14 09:49:35 +0700

    Random

--- app/src/main/java/com/example/random/MainActivity.java
+++ app/src/main/java/com/example/random/MainActivity.java
@@ -15,135 +15,129 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText mEdtsomin,mEdtsomax;
-    Button mBtnRandom,mBtnReset,mBtnAddRange;
-    TextView mTvResult;
-    String mKetqua ="";
+    EditText msomin,msomax;
+    Button mbtnRandom, maddRange, mReset;
+    TextView mResult;
     List<Integer> mArrayListRange;
-    String mtextsomin = "";
-    String mtextsomax = "";
     int msMin=0;
     int msMax=0;
+    String mtextMin = "";
+    String mtextMax = "";
+    String mKetqua ="";
     Random mRandom;
-    int mIndexRandom = -1;
-    int mValue=-1;
+    int mindex = -1;
+    int mvalue = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEdtsomax = findViewById(R.id.somax);
-        mEdtsomin = findViewById(R.id.somin);
-        mBtnRandom = findViewById(R.id.btnRandom);
-        mTvResult = findViewById(R.id.ketqua);
-        mBtnReset = findViewById(R.id.btnReset);
-        mBtnAddRange = findViewById(R.id.btnRange);
 
+        // Ánh xạ
+        msomin = findViewById(R.id.somin);
+        msomax = findViewById(R.id.somax);
+        mbtnRandom = findViewById(R.id.btnRandom);
+        maddRange = findViewById(R.id.btnRange);
+        mReset = findViewById(R.id.btnReset);
+        mResult = findViewById(R.id.ketqua);
 
-        // leak memory: rò rỉ vùng nhớ
+        //Khởi tạo mảng và Random
         mArrayListRange = new ArrayList<>();
         mRandom = new Random();
 
-        disableView(mBtnRandom);
+        // Ẩn nút random
+        disableView(mbtnRandom);
 
-        mBtnAddRange.setOnClickListener(new View.OnClickListener() {
+        mbtnRandom.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // lấy thông tin từ edit text
-                String textsomin = mEdtsomin.getText().toString();
-                String textsomax = mEdtsomax.getText().toString();
-                // Kiểm tra chuỗi rỗng
-                if(textsomin.isEmpty() || textsomax.isEmpty()){
-                    //xuất thông báo
+            public void onClick(View v) {
+                if(mArrayListRange.size()>0){
+                    mindex = mRandom.nextInt(mArrayListRange.size());
+                    mvalue = mArrayListRange.get(mindex);
+                    mKetqua += mArrayListRange.size()==1 ? mvalue : mvalue + "-";
+                    mResult.setText(mKetqua);
+                    mArrayListRange.remove(mindex);
+                }else{
+                    Toast.makeText(MainActivity.this,
+                            "Hết số để random",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        maddRange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy dữ liệu từ edit text
+                String textMin = msomin.getText().toString();
+                String textMax = msomax.getText().toString();
+
+                //Kiểm tra đã nhập chưa
+                if(textMin.isEmpty() || textMax.isEmpty()){
                     Toast.makeText(
                             MainActivity.this,
-                            "Bạn chưa nhập đủ thông tin",
+                            "Chưa nhập đủ thông tin",
                             Toast.LENGTH_SHORT
                     ).show();
                     return;
                 }
 
-                //đổi kiểu dữ liệu string -> integer
-                int sMin = Integer.parseInt(textsomin);
-                int sMax = Integer.parseInt(textsomax);
+                //Chuyển đổi dữ liệu
+                int sMin = Integer.parseInt(textMin);
+                int sMax = Integer.parseInt(textMax);
 
-                // Kiểm tra số min không được lớn hơn hoặc bằng số max
+                // Kiểm tra số min có bé hơn hoặc bằng số max
                 if(sMin>=sMax){
                     sMax = sMin+1;
                 }
-                mEdtsomin.setText(String.valueOf(sMin));
-                mEdtsomax.setText(String.valueOf(sMax));
+                msomin.setText(String.valueOf(sMin));
+                msomax.setText(String.valueOf(sMax));
+
+                // Xoá các phần tử trong mảng trước đó
                 mArrayListRange.clear();
 
-                // Thêm phần tử vào mảng
-                 for (int i = sMin; i <= sMax ; i++) {
+                // Nhập phần tử cho mảng
+                for (int i = sMin; i <= sMax; i++) {
                     mArrayListRange.add(i);
                 }
-                 // Ẩn nút AddRange và Edit Text
-                 disableView(mBtnAddRange);
-                 disableView(mEdtsomin);
-                 disableView(mEdtsomax);
-                 enableView(mBtnRandom);
+
+                // Ẩn nút AddRange và các edit text
+                disableView(maddRange);
+                disableView(msomax);
+                disableView(msomin);
+
+                // Hiển thị nút Random
+                enableView(mbtnRandom);
             }
         });
 
-        mBtnRandom.setOnClickListener(new View.OnClickListener() {
+        mReset.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if(mArrayListRange.size()>0){
-                    mIndexRandom = mRandom.nextInt(mArrayListRange.size());
-                    mValue = mArrayListRange.get(mIndexRandom);
-                    mKetqua += mArrayListRange.size() !=1 ? mValue + "-" : mValue;
-                    mTvResult.setText(mKetqua);
-                    mArrayListRange.remove(mIndexRandom);
-                }else{
-                    Toast.makeText(
-                            MainActivity.this,
-                            "Hết giá trị random",
-                            Toast.LENGTH_SHORT
-                    ).show();
-                }
+            public void onClick(View v) {
+                // Xoá edit text
+                msomax.setText("");
+                msomin.setText("");
 
+                // Hiển thị nút addRange và các edit text
+                enableView(maddRange);
+                enableView(msomin);
+                enableView(msomax);
 
-                // Xử lý Random
-                //Random random = new Random();
-                //int value = random.nextInt(sMax-sMin+1)+sMin;
-                //mKetqua += value + " - ";
-                //mTvResult.setText(mKetqua);
-            }
-        });
-        mBtnReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 1: clear mArrayListRange, set edit text thành chuỗi rỗng
+                // Xoá các phần tử mảng
                 mArrayListRange.clear();
-                mEdtsomin.setText("");
-                mEdtsomax.setText("");
-                // 2: Bật các button ẩn
-                enableView(mEdtsomin);
-                enableView(mBtnAddRange);
-                disableView(mBtnRandom);
-                enableView(mEdtsomax);
-                //visibleView(mEdtsomin);
-                //visibleView(mEdtsomax);
-                // 3: Clear các số đã random
-                mTvResult.setText("");
+
+                // Ẩn mút Random
+                disableView(mbtnRandom);
+
+                // Xoá text view
+                mResult.setText("");
             }
         });
     }
-    // Hàm hiển thị
-    private void enableView(View view){
-        view.setEnabled(true);
-    }
-    // Hàm ẩn
     private void disableView(View view){
         view.setEnabled(false);
     }
-    private void visibleView(View view){
-        view.setVisibility(View.VISIBLE);
-    }
-    private void invisibleView(View view){
-        view.setVisibility(View.INVISIBLE);
+    private void enableView(View view){
+        view.setEnabled(true);
     }
 }
